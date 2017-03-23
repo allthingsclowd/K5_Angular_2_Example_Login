4d4e02b545b172761c619fca24725f2a2a3f5161
allthingsclowd <graham@allthingscloud.eu>
2017-03-23 11:57:12 +0000

    removing excess code

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -2,14 +2,12 @@
 
 import { HomeComponent } from './home/index';
 import { LoginComponent } from './login/index';
-import { RegisterComponent } from './register/index';
 import { AuthGuard } from './_guards/index';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
-
+    
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
 ];
